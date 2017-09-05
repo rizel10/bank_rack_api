d9126604fb68f8bdbca8dc9f18c52414ca6de416
Lucas Wieloch <lucasrizel@ioasys.com.br>
2017-09-05 16:14:59 -0300

    Provided ways for each controller and action to access important data to complete requests

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,10 +2,14 @@
 
 class BaseController
   attr_reader :env
+  attr_reader :body
   attr_reader :response
 
   def initialize env
     @env = env
-    @response = Response.new({ "content-type" => "application/json" })
+    @response = Response.new({ "content-type" => "application/json" }) # Adds content-type header for any controllers, since for now I'll only work with JSON
+    
+    body_tmp = Rack::Request.new(env).body.read
+    @body = body_tmp == "" ? {} : JSON[body_tmp] # provides body content to controllers, also parses them to JSON, since for now I'll only be working with JSON
   end
 end
--- app/controllers/operation_controller.rb
+++ app/controllers/operation_controller.rb
@@ -1,15 +1,24 @@
 class Operation < BaseController
 
   def create
-    response.body = JSON[{ response: "Create Operation Withdraw/Deposit" }]
+    operation = env["PATH_INFO"].split("/").last
+    response.body = JSON[{ response: "Create #{operation} Operation for Account number: #{account_number} with ammount #{body["ammount"]}" }]
     response.status_code = 201
     return response
   end
 
   def index
-    response.body = JSON[{ response: "List Operations" }]
+    response.body = JSON[{ response: "List Operations of account #{account_number}" }]
     response.status_code = 200
     return response
   end
+
+  private
+
+    def account_number
+      return @account_number if @account_number
+      return @account_number = env["PATH_INFO"].match(/\d+/).to_s.to_i # Type converted to integer so method is consistent with it's name.
+    end
+
 
 end
